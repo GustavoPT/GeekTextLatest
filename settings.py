b5c6899847c2f6846fbf3a00d0c632e7782d9094
GustavoPT <gustxw@gmail.com>
2019-07-27 18:38:36 -0400

    removed add book button

--- settings.py
+++ settings.py
@@ -21,11 +21,11 @@ from functools import wraps
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\xdf\xec\xf0e\x96@h\xa8\xc9\xf9\xbe\x0b\xac^\x0ci[\x17\xa6\xb8/H<\x94'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:team6geek@localhost/geek_text"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:team6geek@localhost/geek_text"
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:YES@localhost/geek_text"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:aa09@localhost/geek_text"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:aa09@localhost/geek_text"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://bce5ce263e3ba7:1543b1ce@us-cdbr-iron-east-02.cleardb.net/heroku_e86cfb095c1e8fa"
 
 db = SQLAlchemy(app)
@@ -35,4 +35,4 @@ migrate = Migrate(app, db)
 connection = mysql.connector.connect(host='localhost',
                                      database='geek_text',
                                      user='root',
-                                     password='team6geek')
+                                     password='aa09')
